ba0f016f3ffb81c829cd7eb0d5cdb610cc08fcc6
tolguru <rnjsehdgks01@naver.com>
2026-01-16 21:15:40 +0900

    Caps Lock만 남김

--- keyboards/keychron/k8_max/ansi/rgb/keymaps/default/keymap.c
+++ keyboards/keychron/k8_max/ansi/rgb/keymaps/default/keymap.c
@@ -50,16 +50,16 @@ void aten_number_swap(uint16_t portNumber);
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_ansi_87 (
-         KC_ESC,			    KC_F1,		KC_F2,		KC_F3,	KC_F4,		KC_F5,		KC_F6,	KC_F7,		KC_F8,		KC_F9,		KC_F10,		KC_F11,		KC_F12,					KC_PSCR,	KC_SCRL,	KC_PAUSE,
-         LT(WINDOWS, KC_GRV),	KC_1,		KC_2,		KC_3,	KC_4,		KC_5,		KC_6,	KC_7,		KC_8,		KC_9,		KC_0,		KC_MINS,	KC_EQL,		KC_BSPC,	KC_INS,		KC_HOME,	KC_PGUP,
-         KC_TAB,			    KC_Q,		KC_W,		KC_E,	KC_R,		KC_T,		KC_Y,	KC_U,		KC_I,		KC_O,		KC_P,		KC_LBRC,	KC_RBRC,	KC_BSLS,	KC_DEL,		KC_END,		KC_PGDN,
-         TD(MACRO_LAYER),	    KC_A,		KC_S,		KC_D,	KC_F,		KC_G,		KC_H,	KC_J,		KC_K,		KC_L,		KC_SCLN,	KC_QUOT,	KC_ENT,
-         KC_LSFT,			    KC_Z,		KC_X,		KC_C,	KC_V,		KC_B,		KC_N,	KC_M,		KC_COMM,	KC_DOT,		KC_SLSH,	KC_RSFT,										KC_UP,
-         KC_LCTL,			    KC_LGUI,	KC_LALT,	KC_SPC,	KC_RALT,	MO(FN),		KC_APP,	MO(WINDOWS),																		KC_LEFT,	KC_DOWN,	KC_RGHT
+         KC_ESC,			KC_F1,		KC_F2,		KC_F3,	KC_F4,		KC_F5,		KC_F6,	KC_F7,		KC_F8,		KC_F9,		KC_F10,		KC_F11,		KC_F12,					KC_PSCR,	KC_SCRL,	KC_PAUSE,
+         KC_GRV,			KC_1,		KC_2,		KC_3,	KC_4,		KC_5,		KC_6,	KC_7,		KC_8,		KC_9,		KC_0,		KC_MINS,	KC_EQL,		KC_BSPC,	KC_INS,		KC_HOME,	KC_PGUP,
+         KC_TAB,			KC_Q,		KC_W,		KC_E,	KC_R,		KC_T,		KC_Y,	KC_U,		KC_I,		KC_O,		KC_P,		KC_LBRC,	KC_RBRC,	KC_BSLS,	KC_DEL,		KC_END,		KC_PGDN,
+         TD(MACRO_LAYER),	KC_A,		KC_S,		KC_D,	KC_F,		KC_G,		KC_H,	KC_J,		KC_K,		KC_L,		KC_SCLN,	KC_QUOT,	KC_ENT,
+         KC_LSFT,			KC_Z,		KC_X,		KC_C,	KC_V,		KC_B,		KC_N,	KC_M,		KC_COMM,	KC_DOT,		KC_SLSH,	KC_RSFT,										KC_UP,
+         KC_LCTL,			KC_LGUI,	KC_LALT,	KC_SPC,	KC_RALT,	MO(FN),		KC_APP,	MO(WINDOWS),																		KC_LEFT,	KC_DOWN,	KC_RGHT
     ),
     [WINDOWS] = LAYOUT_ansi_87 (
        	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,				_______,	_______,	_______,
-       	_______,	ATEN_1_ON,	ATEN_2_ON,	ATEN_3_ON,	ATEN_4_ON,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,
+       	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,
         _______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,
        	_______,	ATEN_1_ON,	ATEN_2_ON,	ATEN_3_ON,	ATEN_4_ON,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,
        	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,										_______,
@@ -75,11 +75,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [FN] = LAYOUT_ansi_87 (
        	TD(OS_FLAG_TOGGLE),	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,				QK_BOOTLOADER,	NK_TOGG,	ATEN_ON,
-       	_______,			BT_HST1,	BT_HST2,	BT_HST3,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,
-        RGB_TOG,			RGB_MOD,	RGB_VAI,	RGB_HUI,	RGB_SAI,	RGB_SPI,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,
+       	_______,			BT_HST1,	BT_HST2,	BT_HST3,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,		_______,	_______,
+        RGB_TOG,			RGB_MOD,	RGB_VAI,	RGB_HUI,	RGB_SAI,	RGB_SPI,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,		_______,	_______,
        	_______,			RGB_RMOD,	RGB_VAD,	RGB_HUD,	RGB_SAD,	RGB_SPD,	_______,	_______,	_______,	_______,	_______,	_______,	_______,
-       	_______,			_______,	_______,	_______,	_______,	BAT_LVL,	NK_TOGG,	_______,	_______,	_______,	_______,	_______,										_______,
-       	_______,			_______,	_______,	_______,	_______,	_______,	_______,	_______,																			_______,	_______,	_______
+       	_______,			_______,	_______,	_______,	_______,	BAT_LVL,	NK_TOGG,	_______,	_______,	_______,	_______,	_______,											_______,
+       	_______,			_______,	_______,	_______,	_______,	_______,	_______,	_______,																			_______,		_______,	_______
     )
 };
 
